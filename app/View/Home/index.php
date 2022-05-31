9c6a281cd25468662d2b5df9801d7a9b427f010d
deden <dedenp014@gmail.com>
2022-05-31 22:07:02 +0700

    add navbar, & add name Tanzilal Gummilang

--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -1,1 +1,7 @@
-<h1>ini halaman home</h1>
+<!-- Jumbotron -->
+<section class="jumbotron text-center pb-0 mt-5">
+  <img src="img/{;}.png" class="rounded-circle img-thumbnail" alt="semicolon" width="250" id="image-profile" />
+  <h1 class="display-4">Tanzilal Gummilang</h1>
+  <p class="lead mb-0 mb-2"><b> 23 years old </b></p>
+  <p class="lead mb-0">PHP Developer</p>
+</section>
